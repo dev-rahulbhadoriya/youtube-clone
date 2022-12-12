f8d68cf2382fa9850d8256ca78234e78afacbcc5
devrahul <rbhadoria97@gmail.com>
2022-12-12 13:11:24 +0530

    adding reducer

---
+++ src/store/index.ts
@@ -0,0 +1,33 @@
+import { configureStore, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import {InitialState} from '../Types'
+import { getHomePageVideos } from './reducers/getHomePageVideos';
+
+const initialState: InitialState ={
+    videos:[],
+    currentPlaying: null,
+    serachTerm :"",
+    serachResults :[],
+    nextPageToken: null,
+    recommendedVideos : []
+};
+
+const YoutubeSlice  = createSlice({
+    name :"youtubeApp",
+    initialState,
+    reducers:{},
+    extraReducers : (builder)=>{
+        builder.addCase(getHomePageVideos.fulfilled, (state, action)=>{
+            // state.videos = action.payload.parsedData;
+            // state.nextPageToken = action.payload.nextPageToken;
+        });
+    },
+})
+
+export const store = configureStore({
+    reducer :{
+        youtubeApp: YoutubeSlice.reducer, 
+    }
+})
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
